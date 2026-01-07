e7f4faf9cf7046c4a4074f4424cc7d88073c711a
KonradHOSCILO <konrad.hoscilo@student.wsb.edu.pl>
2026-01-07 01:22:08 +0100

    settings change database

--- fomo/settings.py
+++ fomo/settings.py
@@ -27,13 +27,12 @@ LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "task_list"
 LOGOUT_REDIRECT_URL = "login"
 
-# Cookies / sesje (domyślne Django działa cookie-based session)
 SESSION_COOKIE_HTTPONLY = True
-CSRF_COOKIE_HTTPONLY = False  # CSRF cookie musi być czytelne dla przeglądarki
+CSRF_COOKIE_HTTPONLY = False
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",  # MUSI być przed AuthenticationMiddleware
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -56,25 +55,36 @@ TEMPLATES = [
     },
 ]
 
-# -------------------------
-# URL / WSGI
-# -------------------------
 ROOT_URLCONF = "fomo.urls"
 WSGI_APPLICATION = "fomo.wsgi.application"
 
-# -------------------------
-# Database (na start SQLite)
-# -------------------------
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
-# -------------------------
-# Static files
-# -------------------------
+OVH_MYSQL_HOST = os.getenv("OVH_MYSQL_HOST")
+OVH_MYSQL_PORT = os.getenv("OVH_MYSQL_PORT", "3306")
+OVH_MYSQL_USER = os.getenv("OVH_MYSQL_USER")
+OVH_MYSQL_PASSWORD = os.getenv("OVH_MYSQL_PASSWORD")
+OVH_MYSQL_DB = os.getenv("OVH_MYSQL_DB")
+
+if OVH_MYSQL_HOST and OVH_MYSQL_USER and OVH_MYSQL_PASSWORD and OVH_MYSQL_DB:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": OVH_MYSQL_DB,
+            "USER": OVH_MYSQL_USER,
+            "PASSWORD": OVH_MYSQL_PASSWORD,
+            "HOST": OVH_MYSQL_HOST,
+            "PORT": OVH_MYSQL_PORT,
+            "OPTIONS": {"charset": "utf8mb4"},
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
